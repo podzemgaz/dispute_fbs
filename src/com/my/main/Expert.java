3ffbd4cfe1776cded9ae4a05952b5d6edcb4ff46
Nicolay Volkov <podzemgaz@gmail.com>
2023-02-25 21:07:39 +0300

    refactor 2

--- src/com/my/main/Expert.java
+++ src/com/my/main/Expert.java
@@ -2,65 +2,49 @@ package com.my.main;
 
 import java.util.Map;
 
-import com.my.main.exception.NoAcceptCodeNumException;
-import com.my.main.exception.NoMismatchCodeNumException;
-import com.my.main.exception.NoValuesException;
-import com.my.main.exception.NoWarehouseException;
-
 public class Expert {
 	private String[][] values; // values of parsing of the dispute text
 	private Map<String, Integer> codeNumAccepted; // values of parsing act of accepting pdf
 	private Map<String, Integer> codeNumMismatch; // values of parsing act of mismatch pdf
 	private String warehouse; // value from parsing of act pdf
+
 	public String[][] getValues() {
 		return values;
 	}
+
 	public void setValues(String[][] values) {
 		this.values = values;
 	}
+
 	public Map<String, Integer> getCodeNumAccepted() {
 		return codeNumAccepted;
 	}
+
 	public void setCodeNumAccepted(Map<String, Integer> codeNumAccepted) {
 		this.codeNumAccepted = codeNumAccepted;
 	}
+
 	public Map<String, Integer> getCodeNumMismatch() {
 		return codeNumMismatch;
 	}
+
 	public void setCodeNumMismatch(Map<String, Integer> codeNumMismatch) {
 		this.codeNumMismatch = codeNumMismatch;
 	}
+
 	public String getWarehouse() {
 		return warehouse;
 	}
+
 	public void setWarehouse(String warehouse) {
 		this.warehouse = warehouse;
 	}
-	
-	public Posting[] getPostings() throws NoValuesException, NoAcceptCodeNumException, NoMismatchCodeNumException, NoWarehouseException {
-		
-		// if values == null it needs to make a request
-		
-		if (values == null) {
-			throw new NoValuesException();
-		}
-		
-		if (codeNumAccepted == null) {
-			throw new NoAcceptCodeNumException();
-		}
-		
-		if (codeNumMismatch == null) {
-			throw new NoMismatchCodeNumException();
-		}
-		
-		if (warehouse == null || warehouse.isEmpty()) {
-			throw new NoWarehouseException();
-		}
-		
+
+	public Posting[] getPostings() {
+
 		Posting[] postings = new Posting[values.length];
 		Posting p;
 		String code;
-	
 
 		for (int i = 0; i < postings.length; i++) {
 			p = new Posting();
@@ -73,9 +57,8 @@ public class Expert {
 			p.setWarehouse(warehouse);
 			postings[i] = p;
 		}
-		
+
 		return postings;
 	}
-	
-	
+
 }
--- src/com/my/main/MainConsole.java
+++ src/com/my/main/MainConsole.java
@@ -2,14 +2,8 @@ package com.my.main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Map;
-
 import com.my.main.actinfo.ActTextParser;
 import com.my.main.actinfo.ActType;
-import com.my.main.exception.NoAcceptCodeNumException;
-import com.my.main.exception.NoMismatchCodeNumException;
-import com.my.main.exception.NoValuesException;
-import com.my.main.exception.NoWarehouseException;
 import com.my.main.exception.TextParseEcxeption;
 
 public class MainConsole {
@@ -24,106 +18,58 @@ public class MainConsole {
 
 		File pdfFile;
 
-		Posting[] postings = null;
-
-		while (postings == null) {
+		// check values
+		while (expert.getValues() == null) {
+			cnsler.printLnLB("Enter dispute text: ");
+			String dispute = cnsler.getTextWithinBrackets();
 			try {
-				postings = expert.getPostings();
-			} catch (NoValuesException e1) {
-				// if no values – request values
-				while (expert.getValues() == null) {
-					cnsler.printLnLB("Enter dispute text: ");
-					String dispute = cnsler.getTextWithinBrackets();
-					try {
-						dtp.parse(dispute);
-					} catch (TextParseEcxeption e) {
-						e.printStackTrace();
-					}
-					expert.setValues(dtp.getValues());
-				}
-			} catch (NoAcceptCodeNumException | NoMismatchCodeNumException e) {
-				while (expert.getCodeNumAccepted() == null || expert.getCodeNumMismatch() == null) {
-
-					System.out.println("Enter path");
-					String path = cnsler.getLine();
-					// check path
-					if (!path.endsWith(".pdf")) {
-						cnsler.printLnErrLB("Неверный формат файла, должен быть pdf");
-						continue;
-					}
-
-					pdfFile = new File(path);
-
-					try {
-						acttp.testParse(pdfFile);
-					} catch (FileNotFoundException e1) {
-						e1.printStackTrace();
-					}
-
-					if (acttp.getActType() == ActType.ACCEPT) {
-						expert.setCodeNumAccepted(acttp.getCodeNum());
-					} else if (acttp.getActType() == ActType.MISMATCH) {
-						expert.setCodeNumMismatch(acttp.getCodeNum());
-					} else {
-						continue;
-					}
-
-					acttp.print();
-
-				}
-			} catch (NoWarehouseException e) {
-				expert.setWarehouse(acttp.getWarehouse());
-
-				while (expert.getWarehouse() == null || expert.getWarehouse().isEmpty()) {
-					cnsler.printLnLB("Enter warehouse: ");
-					expert.setWarehouse(cnsler.getLine());
-				}
+				dtp.parse(dispute);
+			} catch (TextParseEcxeption e) {
+				e.printStackTrace();
 			}
+			expert.setValues(dtp.getValues());
 		}
 
-		/*
-		 * while (expert.getValues() == null) { System.out.println("Enter dispute");
-		 * String dispute = cnsler.getTextWithinBrackets(); try { dtp.parse(dispute); }
-		 * catch (TextParseEcxeption e) { e.printStackTrace(); }
-		 * expert.setValues(dtp.getValues()); }
-		 */
+		// check number codes
+		while (expert.getCodeNumAccepted() == null || expert.getCodeNumMismatch() == null) {
 
-		/*
-		 * while (expert.getCodeNumAccepted() == null || expert.getCodeNumMismatch() ==
-		 * null) {
-		 * 
-		 * System.out.println("Enter path"); String path = cnsler.getLine(); // check
-		 * path if (!path.endsWith(".pdf")) {
-		 * cnsler.printLnErrLB("Неверный формат файла, должен быть pdf"); continue; }
-		 * 
-		 * pdfFile = new File(path);
-		 * 
-		 * try { acttp.testParse(pdfFile); } catch (FileNotFoundException e) {
-		 * e.printStackTrace(); }
-		 * 
-		 * if (acttp.getActType() == ActType.ACCEPT) {
-		 * expert.setCodeNumAccepted(acttp.getCodeNum()); } else if (acttp.getActType()
-		 * == ActType.MISMATCH) { expert.setCodeNumMismatch(acttp.getCodeNum()); } else
-		 * { continue; }
-		 * 
-		 * acttp.print();
-		 * 
-		 * }
-		 */
+			System.out.println("Enter path");
+			String path = cnsler.getLine();
+			// check path
+			if (!path.endsWith(".pdf")) {
+				cnsler.printLnErrLB("Неверный формат файла, должен быть pdf");
+				continue;
+			}
 
-		/* expert.setWarehouse(acttp.getWarehouse()); */
+			pdfFile = new File(path);
 
-		// =======================================
+			try {
+				acttp.testParse(pdfFile);
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			}
 
-		/*
-		 * String[][] values = expert.getValues(); Map<String, Integer> acceptCodeNum =
-		 * expert.getCodeNumAccepted(); Map<String, Integer> mismatchCodeNum =
-		 * expert.getCodeNumMismatch(); String warehouse = expert.getWarehouse();
-		 * 
-		 * if (warehouse == null || warehouse.isEmpty()) {
-		 * System.out.println("Enter warehouse"); warehouse = cnsler.getLine();
-		 * expert.setWarehouse(warehouse); }
-		 */
+			if (acttp.getActType() == ActType.ACCEPT) {
+				expert.setCodeNumAccepted(acttp.getCodeNum());
+			} else if (acttp.getActType() == ActType.MISMATCH) {
+				expert.setCodeNumMismatch(acttp.getCodeNum());
+			} else {
+				continue;
+			}
+
+			acttp.print();
+
+		}
+		
+		// check warehouse
+		expert.setWarehouse(acttp.getWarehouse());
+
+		while (expert.getWarehouse() == null || expert.getWarehouse().isEmpty()) {
+			cnsler.printLnLB("Enter warehouse: ");
+			expert.setWarehouse(cnsler.getLine());
+		}
+
+		Posting[] postings = expert.getPostings();
 
 		cnsler.printLikeBoss(System.lineSeparator());
 
