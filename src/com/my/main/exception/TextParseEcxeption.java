4fb8335ada0b593cb6f08e222d70231108f90687
Nicolay Volkov <podzemgaz@gmail.com>
2023-03-04 19:09:24 +0300

    TestParserException:5 move comment

--- src/com/my/main/exception/TextParseEcxeption.java
+++ src/com/my/main/exception/TextParseEcxeption.java
@@ -3,13 +3,10 @@ package com.my.main.exception;
 public class TextParseEcxeption extends Exception {
 
 	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6595833446574693309L;
-
-	/**
 	 * my exception
 	 */
+	
+	private static final long serialVersionUID = -6595833446574693309L;
 
 	public TextParseEcxeption(String message) {
 		super(message);
