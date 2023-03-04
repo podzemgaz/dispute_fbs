7d436badf1cf8830e1592f3518b7e84db66ddcf2
Nicolay Volkov <podzemgaz@gmail.com>
2023-03-04 18:58:00 +0300

    +- TextParseException

--- src/com/my/main/exception/TextParseEcxeption.java
+++ src/com/my/main/exception/TextParseEcxeption.java
@@ -5,33 +5,14 @@ public class TextParseEcxeption extends Exception {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -5499821439177509049L;
+	private static final long serialVersionUID = -6595833446574693309L;
 
-	public TextParseEcxeption() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public TextParseEcxeption(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
-	public TextParseEcxeption(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+	/**
+	 * my exception
+	 */
 
 	public TextParseEcxeption(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
-	public TextParseEcxeption(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	
 }
