217a3f27925bf5bf8c922ce9c4f7fc071012248c
Nicolay Volkov <podzemgaz@gmail.com>
2023-03-04 19:03:31 +0300

    +AppException

---
+++ src/com/my/main/exception/AppException.java
@@ -0,0 +1,14 @@
+package com.my.main.exception;
+
+public class AppException extends Exception {
+
+	/**
+	 * app exception
+	 */
+	private static final long serialVersionUID = -4404643821011728071L;
+
+	public AppException(String message) {
+		super(message);
+	}
+
+}
